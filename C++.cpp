78d89b6c5e1f46bb31bf96bd87a7d482eb9a4cfb
magpie97 <4865213@gmail.com>
2023-01-30 03:53:29 +0900

    두 번째 커밋 및 시간 체크

--- C++.cpp
+++ C++.cpp
@@ -4,5 +4,6 @@ int main()
 {
     std::cout << "my Git space" << std::endl;
     std::cout << "git update time chack" << std::endl;
+    std::cout << "git update second time check" << std::endl;
     return 0;
 }
