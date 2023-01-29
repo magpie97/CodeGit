b42f8878ead8b7c27d0ce6ac726f74f423540b1d
magpie97 <4865213@gmail.com>
2023-01-30 03:02:58 +0900

    testing commit

---
+++ C++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << " Teating" << std::endl;
+    std::cout << "one more" << std::endl;
+    std::cout << "new print string" << std::endl;
+    std::cout << "branch is main" << std::endl;
+    return 0;
+}
